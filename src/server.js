205767dd1b6e063c429c189da761fdebe8f14751
nheyn <nick@heyn.io>
2016-12-28 21:43:33 -0700

    Update Express React Router Binding
    Update express-react-router to 3.0.0-rc2.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 /* @flow */
 import React from 'react';
-import ReactDOM from 'react-dom/server';
 import express from 'express';
 import device from 'express-device';
 import { handleReactRouter } from 'express-react-router';
 
 import router from './router';
-
-import type { $Request as Request, $Response as Response, NextFunction } from 'express';
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 //  --- Create server ---
@@ -27,8 +24,6 @@ app.use(handleReactRouter(router, HtmlPage, (req) => {
     },
   };
 }));
-// $FlowFixMe
-app.use(handleError);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 //  --- Start server ---
@@ -63,34 +58,4 @@ function HtmlPage({ req, reactHtml, error, ...otherProps }: *): React.Element<*>
       </body>
     </html>
   );
-}
-
-function handleError(err: Error, req: Request, res: Response, next: NextFunction) {
-  // Handle error
-  console.error('[Server Error]', err.stack);
-  if(req.accepts('json')) {
-    res.status(500).send({
-      error: {
-        name: err.name,
-        message: err.message,
-      }
-    });
-  }
-  else if(req.accepts('html')) {
-    const reactHtml = ReactDOM.renderToStaticMarkup(
-      <div>
-        <h4>Error</h4>
-        <hr />
-        <label>{err.name}</label>
-        <span>{err.message}</span>
-      </div>
-    );
-
-    res.status(500).send(ReactDOM.renderToStaticMarkup(
-      <HtmlPage req={req} reactHtml={reactHtml} />
-    ));
-  }
-  else {
-    res.status(500).send(`[${err.name}]: ${err.message}`);
-  }
 }

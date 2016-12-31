19dbbe6be340bfe810ec12e6d25bc544e78468f3
nheyn <nick@heyn.io>
2016-12-30 22:53:36 -0700

    Add Filter to Indexes Selector
    Add the filter argument to the getItemIndexes selector.

--- src/list/selectors/getItemIndexes.js
+++ src/list/selectors/getItemIndexes.js
@@ -1,11 +1,16 @@
 /* @flow */
 import type { State } from '../reducers/todoItemsReducer';
 
-export default function getItemIndexes(state: State): * {
+export default function getItemIndexes(state: State, filter?: 'completed' | 'active'): * {
   const { items, editingItemIndex } = state;
 
   return {
-    indexes: items.map((_, index) => index),
+    indexes: items.map((_, index) => index).filter((index) => {
+      if(filter === 'completed')  return items[index].completed? true: false;
+      if(filter === 'active')     return items[index].completed? false: true;
+
+      return true;
+    }),
     editingItemIndex,
   };
 }
